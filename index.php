814f3fa6a4aee0d4e3c15a55df02b80a03b85522
Andrezdan <andrezdann@gmail.com>
2018-02-22 10:53:03 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,343 @@
+﻿
+<!DOCTYPE html>
+<html lang="fr-FR">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="CV Andreze Toumba Notre Dame du Rosaire IFIAG">
+    <meta name="author" content="Andreze TOUMBA">
+    <link rel="icon" href="favicon/favicon.ico">
+
+    <title>Andreze Toumba</title>
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--animate-->
+    <link rel="stylesheet" type="text/css" href="animate.css">
+
+    <!--hover-->
+    <link href="hover-min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">   
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]-->
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <!--[endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top barr">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand cadr main-logo" href="index.php">Andreze</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse main-menu cadr">
+          <ul class="nav navbar-nav navbar-right cadr">
+            <li><a href="#section1" class="js-scrollTo hvr-underline-from-left">Formations</a></li>
+            <li><a href="#section2" class="js-scrollTo hvr-underline-from-left">Expériences</a></li>
+            <li><a href="#section3" class="js-scrollTo hvr-underline-from-left">Compétences</a></li>
+            <li><a href="#section4" class="js-scrollTo hvr-underline-from-left">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!--section00-->
+    <div id="section00">
+      <div class="container">
+        <div class="row main-titre">
+            <h2 class="animated bounceIn" align="center">Technicien Spécialisé</h2>
+            <h1 class="wow" align="center">DEVELOPPEMENT INFORMATIQUE</h1>
+
+            <div class="main-bouton">
+              <p align="center"><a href="#" class="btn btn-primary my-buton">Télécharger mon cv</a></p>
+            </div>
+          
+        </div>
+        
+      </div>
+    </div> <!--section00-->
+
+  
+    <!--section1-->
+    <div id="section1" class="container-fluid cadr">
+      <div class="row ligne-formation">
+        <h2 align="center">FORMATIONS</h2>
+        <p align="center">Mon parcours académique</p>        
+      </div><!--row1-->
+
+      <div class="row ligne-formation-img">
+        <div class="col-lg-4 wow rollIn">
+          <p align="center"><img src="image/bacc.jpg" alt="bac" class="img-circle my-img img-responsive"></p>
+          <h3 align="center">Baccalauréat Scientifique</h3>
+          <p align="center">Au sein du Lycée Notre Dame du Rosaire à Pointe-Noire</p>
+        </div>
+
+        <div class="col-lg-4 wow flipInX">
+          <p align="center"><img src="image/lang.jpg" alt="langue" class="img-rounded my-img-lang img-responsive cadr"></p>
+          <h3 align="center">English Elementry Degree</h3>
+          <p align="center">Au sein du Centre DISCOVER-AMERICA à Brazzaville</p>
+          
+        </div>
+
+        <div class="col-lg-4 wow fadeInDown">
+          <p align="center"><img src="image/dev.jpg" alt="developpement" class="img-circle my-img img-responsive cadr"></p>
+          <h3 align="center">DTS en développement Informatique</h3>
+          <p align="center">Au sein de l'Institut IFIAG à Casablanca</p>
+          
+        </div>
+        
+      </div>   
+      
+
+
+
+    </div><!--section1-->
+
+
+
+    <!--section2-->
+    <div id="section2" class="container cadr">
+      <div class="row ligne-experiences">
+        <h2 align="center">EXPERIENCES</h2>
+        <p align="center">Mes Expériences Professionelles</p>        
+      </div><!--row1-->
+
+      <div class="row ligne-experiences-img cadr">
+        <div class="col-lg-4 wow rollIn cadr">
+          <h4 align="center">Avril 2015 - Juillet 2015</h4>
+          <p align="center">Stagiaire au département informatique des Assurances Générales du Congo-Vie (AGC-Vie)</p>
+        </div>
+        <div class="col-lg-6 wow rollIn cadr">
+          <p><img src="image/agc.jpg" alt="bac" class="img-rounded my-img img-responsive cadr"></p>
+        </div>
+      </div>
+
+        <div class="row ligne-experiences-img cadr">
+        
+         <div class="col-lg-8 wow fadeInDown cadr">
+          <p><img src="image/logo-organium.jpg" style="float: right;" alt="developpement" class="img-rounded my-img img-responsive cadr"></p>
+          
+          </div>
+
+          <div class="col-lg-4 wow fadeInDown cadr">
+           <h4 align="center">Mars 2017 - Mai 2017</h4>
+          <p align="center">Stagiaire au département informatique de la société Organium-Group</p>
+          
+        </div>
+        
+      </div>  
+
+
+    </div><!--section2-->
+
+
+
+    <!--section3-->
+    <div id="section3" class="container-fluid cadr">
+      <div class="row ligne-formation">
+        <h2 align="center">COMPETENCES</h2>
+        <p align="center">Mes Compétences Informatques</p>        
+      </div><!--row1-->
+
+      <div class="row ligne-formation-img">
+        <div class="col-lg-3 wow rollIn">
+          <h3 align="center">Bureautique & Design</h3>
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"><span class="sr-only-focusable" style="color: #FFFFFF">Microsoft Office</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only-focusable" style="color: #FFFFFF">Adobe Photoshop</span></div>
+          </div>
+
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only-focusable" style="color: #FFFFFF">Adobe Illustrator</span></div>
+          </div>
+
+
+
+        </div>
+
+        <div class="col-lg-3 wow flipInX">
+          <h3 align="center">Système d'Information</h3>
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"><span class="sr-only-focusable" style="color: #FFFFFF">Merise</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="35" aria-valuemin="0" aria-valuemax="100" style="width: 45%;"><span class="sr-only-focusable" style="color: #FFFFFF">UML</span></div>
+          </div>
+          
+        </div>
+
+        <div class="col-lg-3 wow fadeInDown">
+          <h3 align="center">Programmation</h3>
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%;"><span class="sr-only-focusable" style="color: #FFFFFF">Programmation Structurée</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="87" aria-valuemin="0" aria-valuemax="100" style="width: 87%;"><span class="sr-only-focusable" style="color: #FFFFFF">Programmation Evénementielle</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="89" aria-valuemin="0" aria-valuemax="100" style="width: 89%;"><span class="sr-only-focusable" style="color: #FFFFFF">Programmation Orientée Objet</span></div>
+          </div>
+          
+        </div>
+
+        <div class="col-lg-3 wow fadeInDown">
+          <h3 align="center">Systèmes et Réseaux</h3>
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="98" aria-valuemin="0" aria-valuemax="100" style="width: 98%;"><span class="sr-only-focusable" style="color: #FFFFFF">Windows 7 / 8 / 10 </span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"><span class="sr-only-focusable" style="color: #FFFFFF">Windows Server 2012</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%;"><span class="sr-only-focusable" style="color: #FFFFFF">Linux</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"><span class="sr-only-focusable" style="color: #FFFFFF">Administration Réseaux (Routing & Switching)</span></div>
+          </div>
+
+          <div class="progress">
+            <div class="progress-bar  c hvr-bounce-to-right" role="progressbar"  aria-valuenow="97" aria-valuemin="0" aria-valuemax="100" style="width: 97%;"><span class="sr-only-focusable" style="color: #FFFFFF">Maintenance des ordinateurs</span></div>
+          </div>
+          
+        </div>
+        
+      </div> 
+
+      <div class="row ligne-formation-img">
+        <h4 align="center">Connaissances Linguistiques</h4>
+          <div align="center">
+		<h2 style=" border:1px solid yellow;color:red;"> SECTION EN MAINTENANCE</h2>
+		<img src="image/maintenance.jpg" alt="section-en-maintenance" width="300" class="img-responsive"/>         
+            
+          </div>
+          </div>
+        
+      </div> 
+      
+
+
+
+    </div><!--section3-->
+
+
+
+    <!--section4-->
+    <div id="section4">
+      <div class="container">
+        <div class="row main-titre-contact">
+            <h2 class="animated bounceIn" align="center">Contactez moi !</h2>
+
+            <div class="main-contact">
+              <div class="col-lg-4">
+                <p align="cente">Andreze Toumba</p>
+                <div>
+                  <p><a class="main-a-contact hvr-grow-shadow" href="#section1">Formations</a></p>
+                  <p><a class="main-a-contact hvr-grow-shadow" href="#section2">Expériences</a></p>
+                  <p><a class="main-a-contact hvr-grow-shadow" href="#section3">Compétences</a></p>
+                </div>
+              </div>
+
+              <div class="col-lg-4"><p align="center">J'ai l'honneur de recevoir vos mails</p>
+                <p align="center">
+                  <form class="form-group jumbotro" autocomplete="offset">
+                    <input class="form-control" type="text" name="nom" placeholder="Votre Nom" required="required">
+                    <input class="form-control" type="email" name="mail" placeholder="Votre Adresse e-mail" required="required">
+                    <input class="form-control" type="tel" name="phone" placeholder="Votre Numéro de téléphone" required="required">
+                    <textarea class="form-control" name="message" placeholder="Vote message" rows="6" required="required"></textarea>
+                    <input align="center" class="btn btn-success my-buton-envoyer" type="submit" name="formenvoyer" value="Envoyer">
+                  </form>
+                </p>
+              </div>
+
+              <div class="col-lg-4">
+                <p align="center">Réseaux Sociaux</p>
+
+                <div>
+                  
+
+                  <p align="center"><a href=""><i title="+212 6 87 99 45 08" style="font-size:50px; color: #449D44;" class="fa hvr-grow-shadow">&#xf232;</i></a></p>
+
+                  <p align="center"><a href=""><i title="andreze toumba" style="font-size:50px;color: white;" class="fa hvr-grow-shadow">&#xf230;</i></a></p>
+
+                  <p align="center"><a href=""><i title="andrezdannizi" style="font-size:50px;color: red;" class="fa hvr-grow-shadow">&#xf16d;</i></a></p>
+
+                  <p align="center"><a href=""><i title="andrezdannizi" style="font-size:50px;color: yellow" class="fa hvr-grow-shadow">&#xf2ad;</i></a></p>
+
+                  <p align="center"><a href=""><i title="andreze.toumba29" style="font-size:50px;color: #56b6ff;" class="fa hvr-grow-shadow">&#xf17e;</i></a></p>
+                
+
+
+                </div>
+              </div>
+            </div>
+          
+        </div>
+        
+      </div>
+    </div> <!--section00-->
+
+
+
+
+
+   
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    
+
+    <!--wow.js-->
+    <script src="WOW-master/dist/wow.min.js"></script>
+    <script>
+      new WOW().init();
+    </script>
+
+    <!--Scroll-bar-->
+    <script>
+     $(document).ready(function() {
+        $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+            var page = $(this).attr('href'); // Page cible
+            var speed =1200; // Durée de l'animation (en ms)
+            $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+            return false;
+          });
+        });
+    
+    </script>
+
+
+</body>
+</html>
